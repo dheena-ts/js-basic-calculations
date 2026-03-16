4b745bad6638794a5942245c72c5c4bb64390226
dheena-ts <dheena.ts@mitrahsoft.in>
2026-03-16 13:50:49 +0530

    Modified code

--- math.js
+++ math.js
@@ -9,11 +9,17 @@ function subtract(a,b){
     return a-b
 }
 
+
 //multiply
 function multiply(a,b){
     return a*b
 }
 
+function divide(a,b){
+    return a/b
+}
+
 console.log(add(10,20))
 console.log(subtract(10,20))
 console.log(multiply(10,20))
+console.log(divide(10,20))
